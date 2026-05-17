406ae1dc9d438f1c11ed9267b29e27beecbe92a4
Akilesh <achuakilesh04@gmail.com>
2026-05-17 10:56:31 +0530

    Simplyfly

---
+++ Simplyfly/src/main/java/com/simplyfly/SimplyflyApplication.java
@@ -0,0 +1,7 @@
+package com.simplyfly;
+
+public class SimplyflyApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Simplyfly/src/main/java/com/simplyfly/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.simplyfly.config;
+
+public class SecurityConfig {
+}
